b61b74d0b733169c714078ad69ceb40cb30ebe90
Yana <cavaljn@icloud.com>
2023-09-26 20:39:03 +0200

    2 task change and 3 task add

--- main.js
+++ main.js
@@ -44,36 +44,109 @@ const calculator = {
 
 //1 without bind, call or apply
 const obj = {
-  from: 1,
-  to: 10,
-};
-
-obj.createArr = function () {
-  if (this.from <= this.to) {
-    this.arr = Array.from({ length: this.to - this.from + 1 }, (_, i) => this.from + i);
-    this.arr.sort((a, b) => a - b);
-  } else {
-    console.error("error");
+    from: 1,
+    to: 10,
+  };
+  
+  function createArr() {
+    const arr = [];
+  
+    for (let i = this.from; i < this.to; i++) {
+      arr.push(i);
+    }
+  
+    this.arr = arr;
   }
-};
+  
+  obj.createArr = createArr;
+  obj.createArr();
+  
+  const createArrBind = createArr.bind(obj);
+  createArrBind();
+  
+  createArr.call(obj);
+  createArr.apply(obj);
+  
+  console.log(obj);
 
 
-const createArrBind = obj.createArr.bind(obj);
-createArrBind(); 
-console.log(obj.arr);
-
-
-const createArrCall = obj.createArr.call(obj);
-console.log(obj.arr);
-
-
-const createArrApply = obj.createArr.apply(obj);
-console.log(obj.arr);
-
-
-obj.createArr();
-console.log(obj.arr);
-
+  //constructor function
+  function Car(model, color, age, speed, gasTank, started) {
+    const maxSpeed = 200;
+    const maxGas = 20;
+  
+    function speedUp(newSpeed) {
+      const started = this.started;
+      const emptyGas = this.checkGasIsEmpty();
+  
+      if (started && !emptyGas) {
+        const gasLevel = this.gasTank - 5;
+        const speedCalc = this.speed + newSpeed;
+        this.speed = speedCalc <= maxSpeed ? speedCalc : maxSpeed;
+        this.gasTank = gasLevel < 0 ? 0 : gasLevel;
+        gasLevel === 0 && this.stop();
+      } else {
+        this.stop();
+      }
+  
+      return this;
+    }
+    function startEngine() {
+      this.started = !this.checkGasIsEmpty();
+      return this;
+    }
+  
+    this.model = model;
+    this.color = color;
+    this.age = age;
+    this.speed = speed;
+    this.gasTank = gasTank;
+    this.started = started;
+    this.startEngine = startEngine;
+    this.drive = function drive() {
+      const started = this.started;
+      const emptyGas = this.checkGasIsEmpty();
+      if (started && !emptyGas) {
+        this.speed = 30;
+      }
+      return this;
+    };
+    this.stop = function stop() {
+      this.speed = 0;
+      this.started = false;
+  
+      return this;
+    };
+    this.speedUp = speedUp;
+    this.slowDown = function slowDown(newSpeed) {
+      const started = this.started;
+      const emptyGas = this.checkGasIsEmpty();
+  
+      if (started && !emptyGas) {
+        const gasLevel = this.gasTank - 5;
+        const speedCalc = this.speed - newSpeed;
+        this.speed = speedCalc <= 0 ? 0 : speedCalc;
+        this.gasTank = gasLevel < 0 ? 0 : gasLevel;
+        gasLevel === 0 && this.stop();
+      } else {
+        this.stop();
+      }
+  
+      return this;
+    };
+    this.addGas = function addGas(newLevel) {
+      const gasLevel = this.gasTank + newLevel;
+      this.gasTank = gasLevel > maxGas ? 20 : gasLevel;
+  
+      return this;
+    };
+    this.checkGasIsEmpty = function () {
+      return this.gasTank === 0;
+    };
+  }
+  
+  const car = new Car("BMW", "White", 5, 0, 20, false);
+  console.log(car);
 
 
   
