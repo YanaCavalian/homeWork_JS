53e389a2030424c7567b0cb5ec87d6a417d32f5b
Yana <cavaljn@icloud.com>
2023-10-06 21:37:54 +0200

    last change

--- main.js
+++ main.js
@@ -42,25 +42,10 @@ const randomResolve = () => {
         console.error(errors); 
       });
   };
-  
- 
   createPromiseArr(4);
   
   //Async/Await
-
-  const randomResolve1 = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (Math.random() < 0.5) {
-          resolve('Resolved');
-        } else {
-          reject('Rejected');
-        }
-      }, Math.random() * 1000);
-    });
-  };
-  
-  const printResponse = async (n) => {
+  const createPromiseArr = async (n) => {
     const promises = [];
   
     for (let i = 0; i < n; i++) {
@@ -69,54 +54,73 @@ const randomResolve = () => {
   
     try {
       const results = await Promise.all(promises);
-      console.log('🥳 Yasss');
+      console.log('🥳 Ура');
       console.log(results);
+      return results;
     } catch (errors) {
-      console.log('🫠🫡 ok');
+      console.log('🫠🫡 Ошибка');
       console.error(errors);
+      throw errors;
+    }
+  };
+  
+  const printResponse = async (n) => {
+    try {
+      const results = await createPromiseArr(n);
+      console.log('🥳 Ура');
+      console.log(results);
+    } catch (error) {
+      console.log('🫠🫡 Ошибка');
+      console.error(error);
     }
   };
   
   printResponse(4);
-  
+   
   //Casino (optional)
   const getRandomSeconds = () => Math.floor(Math.random() * 5) + 1;
 
-const resolvePromiseWithDelay = (value) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const color = value % 2 === 0 ? 'red' : 'black';
-      resolve({ value, color });
-    }, getRandomSeconds() * 1000);
-  });
-};
-
-const casino = async (stakes) => {
-  const promises = stakes.map((stake) => {
-    if (stake.value === 'red') {
-      return resolvePromiseWithDelay(2); 
-    } else if (stake.value === 'black') {
-      return resolvePromiseWithDelay(1); 
-    } else {
-      const number = parseInt(stake.value);
-      return resolvePromiseWithDelay(number);
+  const resolvePromiseWithDelay = (value) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        let color;
+        if (value === 0) {
+          color = 'green';
+        } else {
+          color = value % 2 === 0 ? 'red' : 'black';
+        }
+        resolve({ value, color });
+      }, getRandomSeconds() * 1000);
+    });
+  };
+  
+  const casino = async (stakes) => {
+    const promises = stakes.map((stake) => {
+      if (stake.value === 'red') {
+        return resolvePromiseWithDelay(2); 
+      } else if (stake.value === 'black') {
+        return resolvePromiseWithDelay(1); 
+      } else {
+        const number = parseInt(stake.value);
+        return resolvePromiseWithDelay(number);
+      }
+    });
+  
+    try {
+      const result = await Promise.race(promises);
+      console.log(`💵 (${result.value} - ${result.color})`); 
+    } catch (error) {
+      console.log('🫣'); 
     }
-  });
-
-  try {
-    await Promise.race(promises); 
-    console.log('💵'); 
-  } catch (error) {
-    console.log('🫣'); 
-  }
-};
-
-const stakes = [
-  { value: 'red' },
-  { value: 'black' },
-  { value: '1' },
-  { value: '2' },
-  { value: '0' }
-];
-
-casino(stakes);
+  };
+  
+  const stakes = [
+    { value: 'red' },
+    { value: 'black' },
+    { value: '1' },
+    { value: '2' },
+    { value: '0' }
+  ];
+  
+  casino(stakes);
+  
